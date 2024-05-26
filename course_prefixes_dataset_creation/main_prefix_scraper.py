4f88420753541232da58357e6d0d0a306818b867
Arav Luthra <aravluthra@aravs-air.lan>
2024-05-26 19:13:08 -0400

    changes added

--- course_prefixes_dataset_creation/main_prefix_scraper.py
+++ course_prefixes_dataset_creation/main_prefix_scraper.py
@@ -1,6 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 import csv
+import pandas
 
 # URL of the University of Maryland's approved courses page
 url = 'https://academiccatalog.umd.edu/undergraduate/approved-courses/'
@@ -30,6 +31,7 @@ def separate_course_prefix(full_course_prefix: str):
     words = full_course_prefix.split(' - ')
     return words[0], words[1]
 
+
 with open('umd_course_prefixes.csv', 'w', newline='') as csvfile:
     writer = csv.writer(csvfile)
     writer.writerow(['Course Prefix', 'Full Form'])
@@ -39,3 +41,35 @@ with open('umd_course_prefixes.csv', 'w', newline='') as csvfile:
         writer.writerow([course_prefix, full_form])
 
 print('Data successfully written to umd_course_prefixes.csv')
+
+
+# URL of the University of Maryland's approved courses page
+url = 'https://app.testudo.umd.edu/soc/'
+
+# Send a GET request to the page
+response = requests.get(url)
+response.raise_for_status()  # Check that the request was successful
+
+# Parse the HTML content of the page
+soup = BeautifulSoup(response.content, 'html.parser')
+
+# Dictionary to hold course prefixes and their full forms
+course_dict = {}
+
+# Find all course prefixes and their descriptions
+course_prefixes = soup.find_all('div', class_='course-prefix row')
+
+with open('umd_course_prefixes.csv', 'a', newline='') as csvfile:
+    writer = csv.writer(csvfile)
+    df = pandas.read_csv('umd_course_prefixes.csv')
+    existing_course_prefixes = set(df["Course Prefix"])
+
+    for entry in course_prefixes:
+        course_prefix = entry.find('span', class_='prefix-abbrev push_one two columns').text.strip()
+
+        if course_prefix not in existing_course_prefixes:
+            full_form = entry.find('span', class_='prefix-name nine columns').text.strip()
+            writer.writerow([course_prefix, full_form])
+
+print('Data successfully appended to schedule_course_prefixes.csv')
+
